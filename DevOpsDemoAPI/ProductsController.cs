cefb339da8b9b44ce1c4a168419bb7fc5ca7de26
eduardo <eduardo.bc.pb@gmail.com>
2026-04-12 19:33:35 -0600

    Initial commit

---
+++ DevOpsDemoAPI/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsDemoAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Laptop", Price = 1200 },
+                new { Id = 2, Name = "Mouse", Price = 25 }
+            };
+
+            return Ok(products);
+        }
+    }
+}
